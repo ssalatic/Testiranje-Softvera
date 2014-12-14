517829287f5b986be376ddd4e0c33257406cf056
Sladjan <skantar12@gmail.com>
2014-12-14 11:27:00 +0100

    Menu
    Prikaz menija u zavisnosti od korisnika
    User ne vidi Kostimi i koreografije
    Trener ne vidi kostimi
    Dizajner ne vidi koreografije
    Admin vidi sve

--- Laravel/app/models/MenuModel.php
+++ Laravel/app/models/MenuModel.php
@@ -5,11 +5,12 @@
 class MenuModel extends \Eloquent{
 	
 	public static function updateMenu($route, $user){
-	
 		$menu_items = array("Home", "Practices", "Concerts", "Users", "Competitions", "Costumes", "Choreographies", "Files" );
 		$menu_routes = array(route('index'), route('trainings.index'), route('concerts.index'), route('users.index'), route('competitions.index'), route('costumes.index'), route('choreographies.index'), route('gallery') );
 	
 		for($i = 0; $i < count($menu_items); ++$i) {
+		if($i === 5 && (!$user->isAdmin() && !$user->isDesigner())) continue;
+		if($i === 6 && (!$user->isAdmin() && !$user->isTrainer())) continue;
 			if( URL::route($route) === $menu_routes[$i])
 				echo '<li class="active" ><a href="'.$menu_routes[$i].'">'.$menu_items[$i].'</a></li>';
 			else

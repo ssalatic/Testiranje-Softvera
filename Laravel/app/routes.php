8f9d32e1a1f32c73155d3a05c2379a58efc84d2c
illusi0n <miloshb92@hotmail.com>
2015-01-09 02:17:13 +0100

    Choreographies strana

--- Laravel/app/routes.php
+++ Laravel/app/routes.php
@@ -103,6 +103,11 @@ Route::group(array('before' => 'secure'), function()
 		return $id;
 	}	
 	);
+	
+	Route::post('addToKnows', array('before' => 'secure|auth', 'https' => true , 'as' => 'addToKnows', 'uses' => 'ChoreographyController@addToKnows'));
+	Route::post('removeFromKnows', array('before' => 'secure|auth', 'https' => true , 'as' => 'removeFromKnows', 'uses' => 'ChoreographyController@removeFromKnows'));
+	
+	
 	Route::get('login', array('before' => 'secure|isAuth', 'https' => true , 'as' => 'login', 'uses' => 'PagesController@login'));
 	Route::get('gallery', array('before' => 'secure|auth', 'https' => true , 'as' => 'gallery', 'uses' => 'PagesController@gallery'));
 	Route::get('function', array('before' => 'secure|auth', 'https' => true , 'as' => 'function', 'uses' => 'PagesController@get_user_func'));
